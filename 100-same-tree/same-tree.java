9de49bc67df8df4a33abe5f913342b5e84b925e7
GOKULAKANNAN <gokula9611@gmail.com>
2026-04-23 22:09:24 +0530

    Time: 0 ms (100.00%) | Memory: 42.9 MB (55.30%) - LeetSync

---
+++ 100-same-tree/same-tree.java
@@ -0,0 +1,12 @@
+class Solution {
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        // Base cases
+        if (p == null && q == null) return true;
+        if (p == null || q == null) return false;
+        if (p.val != q.val) return false;
+
+        // Recursive check
+        return isSameTree(p.left, q.left) &&
+               isSameTree(p.right, q.right);
+    }
+}
